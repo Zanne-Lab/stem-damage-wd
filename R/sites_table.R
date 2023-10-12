ec6e4ee5552e9712bcbaf33d6072ab91380f8600
Habacuc Flores Moreno <habacucfm@gmail.com>
2023-10-12 21:14:05 +1000

    First commit sites table

---
+++ R/sites_table.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(elevatr)
+
+read_csv("data/stem_damage.csv") %>%
+  select(Site, longitude, latitude ,species_matched) %>%
+  distinct(Site,latitude, longitude,species_matched) %>%
+  count(Site,latitude, longitude,name = "SpeciesNumber") ->p
+
+DT <- st_as_sf(x = p, coords = c("longitude","latitude"), remove = FALSE)
+DT <- st_set_crs(DT, value = "WGS84")
+DTT <- get_elev_point(DT, src = "aws")
+
+DT$p2 = st_as_sf(as.data.frame(DT), coords = c("long2","lat2"))
